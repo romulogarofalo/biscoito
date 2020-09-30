8d49d784bc640499ca9fed566e8ea2fc4d2b8096
romulo garofalo <romulogarofalo@gmail.com>
2020-09-30 21:32:34 +0000

    :sparkles: endpoints for Auth

--- lib/biscoito_web/router.ex
+++ lib/biscoito_web/router.ex
@@ -17,6 +17,8 @@ defmodule BiscoitoWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/users", UserController, except: [:new, :edit]
+
   end
 
   # Other scopes may use custom stacks.
